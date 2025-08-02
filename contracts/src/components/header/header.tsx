84fa42a51f715a333aecc03e06f49b9e17958d8e
Yakubenka Ivan <palasja@gmail.com>
2025-08-02 19:56:18 +0300

    fix: fix countdown

--- contracts/src/components/header/header.tsx
+++ contracts/src/components/header/header.tsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router';
 import style from './header.module.css';
 import Countdown from 'react-countdown';
-import { CookiesProvider, useCookies } from 'react-cookie';
+import {  useCookies } from 'react-cookie';
 
 const Header = () => {
-  const [cookies, setCookie] = useCookies(['expireDate'])
+  const [cookies, setCookie] = useCookies(['expireDate']);
+  console.log(cookies.expireDate);
   return (
     <header className="noprint" data-testid="header">
       <nav>
         <Link to={'/contract'}>Договора</Link>
         <Link to={'/act'}>Акты</Link>
         <Link to={'/stats'}>Статистика</Link>
+
       </nav>
-      <Countdown date={cookies.expireDate} />
+      <div data-testid='qwe'>
+<Countdown date={cookies.expireDate} />
+      </div>
+      
     </header>
 
   );
