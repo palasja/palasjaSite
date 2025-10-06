4861f3d4f66d3ad8f4b9405904baf2a6250aefa8
Yakubenka Ivan <palasja@gmail.com>
2025-10-06 16:50:59 +0300

    fix : gusmary change instagramm link

--- gusMaryHairSSR/src/helpers/constants.tsx
+++ gusMaryHairSSR/src/helpers/constants.tsx
@@ -4,7 +4,7 @@ export const RESPONSIVE = {
 };
 export const API = 'https://gusmaryapi.palasja.site';
 // export const API = 'http://localhost:3003';
-export const INSTAGRAM_LINK = 'https://instagram.com/keratin_gusmari_hair/';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/gusak_mariya_hair?igsh=YWRjbmdobmR0OXYw';
 export const PHONE_NUMBER = '+375 29 2377836';
 
 export const MONTH_RU = [
